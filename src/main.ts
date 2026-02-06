aadbd7266cae3fee2f66500c5b4be052aa82faf6
Johan Cappi <johan.cappi@airmes.eu>
2026-02-06 10:44:22 +0100

    Corriger un espace dans le message de démarrage du script

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 
 import { bootstrapExtra, getVariables, initDoors } from "@workadventure/scripting-api-extra";
 
-console.log('Script started successfully');
+console.log('Script started successfully !');
 
 let currentPopup: any = undefined;
 
